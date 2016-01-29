ec33ed534379dc08aab5d1f871d7c30edc6734a6
pradeepyubin <pradeepyubin@gmail.com>
2016-01-29 22:30:04 +0530

    Update integer.c

--- integer.c
+++ integer.c
@@ -13,47 +13,38 @@ void main()
 		          numc=num;
 			      while(numc!=0)
 			        {
-				      numc/=10;
-				            ++count;
-					      }
-					          // printf("\n num = %d",num);
-						       n=count;
+			      numc/=10;
+		            ++count;
+			      }
+			       n=count;
 						        
-							 if(delete<=count)
-							  {
-							       for(i=count-1;i>=0;i--)
-							            	{
-									     		number[i]=num%10;
-											       	    num=num/10;
-												            //	printf("\n %d",number[i]);
-													         	}
+			 if(delete<=count)
+		  {
+		       for(i=count-1;i>=0;i--)
+	            	{number[i]=num%10;
+			  num=num/10;
+									 
+	      	}
 															     	
 																     	
-																	     	 for (i = 0; i < n; ++i)
-																		     {
-																		             for (j = i + 1; j < n; ++j)
-																			             {
-																				                 if (number[i] > number[j])
-																						             {
-																							                     a =  number[i];
-																									                     number[i] = number[j];
-																											                     number[j] = a;
-																													                 }
-																															         }
-																																     }
-
-																																     	printf("\n The least integer after deleting %d digits is",delete);
-																																		
-																																			for(i=0;i<count-delete;i++)
-																																			       	{
-																																						printf("\n %d",number[i]);
-																																						       }
-
-																																						       }
-
-
-																																						       else
-
-																																						       printf("\n No of digits to be deleted is greater than the entered digits of the integer . Try Again");
-																																						       	 
-																																							 }
+			 for (i = 0; i < n; ++i)
+		  {
+		             for (j = i + 1; j < n; ++j)
+		        {
+			                 if (number[i] > number[j])
+			             {
+				                     a =  number[i];
+			                     number[i] = number[j];
+                    		                     number[j] = a;
+		                      }
+			  }
+		  }
+    	printf("\n The least integer after deleting %d digits is",delete);
+			for(i=0;i<count-delete;i++)
+	      	     {
+				printf("\n %d",number[i]);
+		       }
+      }
+       else
+       printf("\n No of digits to be deleted is greater than the entered digits of the integer . Try Again");
+ }
